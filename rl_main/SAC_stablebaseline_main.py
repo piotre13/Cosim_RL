44a14fe8b7d39e5158b2bf8db14521edab01dd0c
piotre13 <pietro.randomazzarino@gmail.com>
2025-08-13 14:40:46 +0200

    RL integration using gym compliant wrapper. starting point for paper with Tsinghua

--- rl_main/SAC_stablebaseline_main.py
+++ rl_main/SAC_stablebaseline_main.py
@@ -2,8 +2,8 @@ from stable_baselines3 import SAC
 
 def main(env):
 
-    model = SAC("MultiInputPolicy", env, learning_rate=0.0003, buffer_size=1000000, learning_starts=100, batch_size=1000,
-                tau=0.005, gamma=0.99, train_freq=10, gradient_steps=1, action_noise=None, replay_buffer_class=None, replay_buffer_kwargs=None,
+    model = SAC("MultiInputPolicy", env, learning_rate=0.0003, buffer_size=1000000, learning_starts=100, batch_size=512,
+                tau=0.005, gamma=0.99, train_freq=1, gradient_steps=1, action_noise=None, replay_buffer_class=None, replay_buffer_kwargs=None,
                 optimize_memory_usage=False, ent_coef='auto', target_update_interval=1, target_entropy='auto', use_sde=False, sde_sample_freq=-1,
                 use_sde_at_warmup=False, stats_window_size=100, tensorboard_log=None, policy_kwargs=None, verbose=1, seed=None, device='auto',
                 _init_setup_model=True)
@@ -20,3 +20,4 @@ def main(env):
 
     env.save_fed_memory()
     env.finalize()
+
